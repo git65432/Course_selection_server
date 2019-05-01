0c84949167723aaa01c40274ac4dbfcb912a2bbb
git65432 <1311273615@qq.com>
2019-05-02 00:00:33 +0800

    111

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 var Mock = require('mockjs');
-
+Mock.Random.extend({
+  addressKf: function() {
+  var addressKf = ['河北省邯郸市', '河南省登封市', '天津市西青区', '北京市昌平区']
+    return this.pick(addressKf)
+    }
+  })
 module.exports = {
   register: Mock.mock({
     "error": 0,
@@ -15,10 +20,18 @@ module.exports = {
     "result": {
       "err": 0,
       "mes":"登录成功",
-      "id|1-100": 100,
-      "teacher|1-2":true,
-      "name":"@name",
-      
+      "id": "@id",
+      "teacher|1":true,
+      "name":"@cname",
+      "address":"@addressKf",
+      "phone":/^1(3|4|5|7|8)\d{9}$/,
+      "email":"@email",
+      "nation|1":["汉","藏","蒙","维","傣","佤","朝鲜"],
+      "school|1":["第一中学","第二中学","第三中学","第四中学"],
+      "grade|1":["1年级","2年级","3年级"],
+      "sex|1":["男","女"],
+      "subject|1":[["语文","书法"],["体育","美术"]],
+      "HeadPortrait":Mock.Random.image('60x60', '#4A7BF7', 'img')
     }
   })
 };
